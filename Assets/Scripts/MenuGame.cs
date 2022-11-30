b360962dade356abfc816b7b7160cae0f441330b
MichDeb36 <michal.debinski36@gmail.com>
2022-11-30 13:44:00 +0100

    the game is fully playable

---
+++ Assets/Scripts/MenuGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Prefabs.Locomotion.Teleporters;
+using Zinnia.Data.Type;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuGame : MonoBehaviour
+{
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("EscapeRoomETI"); 
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    private float timer = 0;
+    private bool checkEnd = false;
+    public Text timerText;
+
+    void Start()
+    {
+
+    }
+
+    public void endGame()
+    {
+        checkEnd = true;
+    }
+
+    void UpdateTime()
+    {
+        timer += Time.deltaTime;
+        int minutes = (int)(timer / 60);
+        int seconds = (int)(timer % 60);
+        timerText.text = string.Format("Czas gry: {0:00}:{1:00}", minutes, seconds);
+    }
+    
+    void Update()
+    {
+        if(checkEnd == false)
+        {
+            UpdateTime();
+        }
+    }
+}
